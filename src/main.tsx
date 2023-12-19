900fe8e2ca87b5aecdaf84454d20942528daaaaa
altq <zhong.li@united-imaging.com>
2023-12-19 14:38:39 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable react-refresh/only-export-components */
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App.tsx';
 import Login from './pages/login';
 import BaseLayout from './layout';
+import Exception404 from './pages/exception/404';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { ConfigProvider } from '@arco-design/web-react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalContext } from './context';
+import useStorage from './utils/useStorage';
 import rootReducer from './store';
+import zhCN from '@arco-design/web-react/es/locale/zh-CN';
+import enUS from '@arco-design/web-react/es/locale/en-US';
+import changeTheme from './utils/changeTheme';
 import '@arco-design/web-react/dist/css/arco.css';
 import '@/style/index.less';
 import './mock';
@@ -17,9 +22,43 @@ import './mock';
 const store = createStore(rootReducer);
 
 function Index() {
+  const [lang, setLang] = useStorage('arco-lang', 'zh-CN');
+  const [theme, setTheme] = useStorage('arco-theme', 'light');
+
+  function getArcoLocale() {
+    switch (lang) {
+      case 'zh-CN':
+        return zhCN;
+      case 'en-US':
+        return enUS;
+      default:
+        return zhCN;
+    }
+  }
+
+  // useEffect(() => {
+  //   if (checkLogin()) {
+  //     fetchUserInfo();
+  //   } else if (window.location.pathname.replace(/\//g, '') !== 'login') {
+  //     window.location.pathname = '/login';
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    changeTheme(theme);
+  }, [theme]);
+
+  const contextValue = {
+    lang,
+    setLang,
+    theme,
+    setTheme
+  };
+
   return (
     <BrowserRouter>
       <ConfigProvider
+        locale={getArcoLocale()}
         componentConfig={{
           Card: {
             bordered: false
@@ -32,11 +71,12 @@ function Index() {
           }
         }}>
         <Provider store={store}>
-          <GlobalContext.Provider value={{}}>
+          <GlobalContext.Provider value={contextValue}>
             <Routes>
               <Route path='/' element={<BaseLayout />}>
-                <Route path='/login' element={<Login />} />
                 <Route path='/' element={<App />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='*' element={<Exception404 />} />
               </Route>
             </Routes>
           </GlobalContext.Provider>
---
+++ src/pages/exception/404.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Result, Button } from '@arco-design/web-react';
+import useLocale from '@/utils/useLocale';
+
+const locale = {
+  'en-US': {
+    'menu.exception': 'Exception page',
+    'menu.exception.404': '404',
+    'exception.result.404.description': 'Whoops, this page is gone.',
+    'exception.result.404.retry': 'Retry',
+    'exception.result.404.back': 'Back'
+  },
+  'zh-CN': {
+    'menu.exception': '异常页',
+    'menu.exception.404': '404',
+    'exception.result.404.description': '抱歉，页面不见了～',
+    'exception.result.404.retry': '重试',
+    'exception.result.404.back': '返回'
+  }
+};
+
+function Exception404() {
+  const t = useLocale(locale);
+
+  return (
+    <>
+      <Result
+        className='result'
+        status='404'
+        subTitle={t['exception.result.404.description']}
+        extra={[
+          <Button key='again' style={{ marginRight: 16 }}>
+            {t['exception.result.404.retry']}
+          </Button>,
+          <Button key='back' type='primary'>
+            {t['exception.result.404.back']}
+          </Button>
+        ]}
+      />
+    </>
+  );
+}
+
+export default Exception404;
